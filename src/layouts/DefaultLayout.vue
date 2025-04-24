f06c1e35a53274f58c2a96b7a56521c2a6640617
Sarah Brady <ydarbharas@gmail.com>
2025-04-24 23:00:08 +0100

    Default layout with navigation

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="min-h-screen bg-gray-50">
+    <AppNav />
+    <main class="py-10">
+      <div class="mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="mb-3">
+          <h1 class="text-2xl font-semibold text-gray-900">{{ title }}</h1>
+          <p v-if="subtitle" class="mt-2 text-md text-gray-700">
+            {{ subtitle }}
+          </p>
+        </div>
+        <div class="min-h-[calc(100vh-12rem)]">
+          <slot />
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts">
+import AppNav from "../components/layout/AppNav.vue";
+
+withDefaults(
+  defineProps<{
+    title: string;
+    subtitle?: string;
+  }>(),
+  {
+    subtitle: "",
+  }
+);
+</script>
