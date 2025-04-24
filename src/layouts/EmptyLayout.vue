9e8ea0489a2505e06ac81ece68cd26a84ffe9c69
Sarah Brady <ydarbharas@gmail.com>
2025-04-24 23:00:35 +0100

    Empty layout setup (for auth)

---
+++ src/layouts/EmptyLayout.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="min-h-screen bg-gray-50">
+    <main class="py-10">
+      <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <!-- Page header -->
+        <div class="mb-8">
+          <h1 class="text-2xl font-semibold text-gray-900">{{ title }}</h1>
+          <p v-if="subtitle" class="mt-2 text-sm text-gray-700">
+            {{ subtitle }}
+          </p>
+        </div>
+
+        <!-- Page content -->
+        <div class="min-h-[calc(100vh-16rem)]">
+          <slot />
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    title: string;
+    subtitle?: string;
+  }>(),
+  {
+    subtitle: "",
+  }
+);
+</script>
